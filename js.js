437efaf330f4bc4bd5811c8340798f289929144e
Dmitrii Benenson <71938599+demonben@users.noreply.github.com>
2021-02-10 17:15:54 +0200

    m3(F)

--- js.js
+++ js.js
@@ -1,20 +1,18 @@
 
-function fibionacci(num4) {
-       
-    let num1 = 1;
-    let num2 = 1;
-    for (let i = 3; i <= num4; i++) {
-      let num3 = num1 + num2;
-      num1 = num2;
-      num2 = num3;
-    }
-    return num2;
+function fibionacci(num4) {   
+  let num1 = 0;
+  let num2 = 1;
+  let num3 = 1
+  for (let i = 2; i <= num4; i++) {
+    let num3 = num1 + num2;
+    num1 = num2;
+    num2 = num3;
   }
-  let num4 = 7
-  let num1 = fibionacci(num4)
-  fibionacci(num4)
+  return num2;
+}
 
-let num2 = num4
 
-document.getElementById('y').innerText = num1
-document.getElementById('x').innerText = num2
+document.getElementById('validateBtn').addEventListener('click',function(){
+  let form = document.getElementById('fname').value
+document.getElementById('y').innerText = fibionacci(form)
+})
