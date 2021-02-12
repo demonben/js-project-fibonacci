741e71733899bde77114a4df4f0cdc3a32ce04ad
Dmitrii Benenson <71938599+demonben@users.noreply.github.com>
2021-02-12 13:15:18 +0200

    m4(inprocess)

--- js.js
+++ js.js
@@ -1,25 +1,50 @@
-
 // fetch
-let getUrl = `"http://localhost:5050/fibonacci/:${}"`
 
-fetch(getUrl).then(function(response){
+// let getUrl = `"http://localhost:5050/fibonacci/:${}"`
+
+// fetch(getUrl).then(function(response){
   
-})
+// })
 
 // fetch
+
+//const input = document.getElementById('fname')
+//let hey = input.value
+
 function fibionacci(num4) {   
   let num1 = 0;
   let num2 = 1;
   let num3 = 1
   for (let i = 2; i <= num4; i++) {
-    let num3 = num1 + num2;
+    num3 = num1 + num2;
     num1 = num2;
     num2 = num3;
   }
-  return num2;
+  return num3;
 }
-
-document.getElementById('validateBtn').addEventListener('click',function(){
-  let form = document.getElementById('fname').value
-document.getElementById('y').innerText = fibionacci(form)
+let button = document.getElementById('btn')
+button.addEventListener('click', function() {
+  
+const num4 = document.getElementById('fname').value
+console.log(num4)
+  document.getElementById('y').innerText = fibionacci(num4)
 })
+
+
+
+
+
+
+// document.getElementById('btn').addEventListener('click',function(){
+//   let form = document.getElementById('fname').value
+// document.getElementById('y').innerText = fibionacci(form)
+// })
+
+
+// raz code
+// function getFibonacci(){
+
+// }
+// const input = document.getElementById('fname')
+
+// button.addEventListener('click', getFibonacci)
